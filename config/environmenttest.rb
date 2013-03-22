a073d4697ff49d53ad5be08704deda364f47758f
wcorbett36 <wcorbett36@gmail.com>
2013-03-22 13:04:16 -0400

    Finish user signup

---
+++ config/environmenttest.rb
@@ -0,0 +1,9 @@
+ampleApp::Application.configure do
+
+
+require 'bcrypt' 
+silence_warnings do  
+   BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
+end
+
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+  factory :user do 
+    name  "William Corbett" 
+    email "wcorbett36@gmail.com" 
+    password "foobar"  
+    password_confirmation "foobar" 
+   end
+  end
